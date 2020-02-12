b520fd378e3c4bea6120b334cfa8f1793c10640d
mrmen <tetcheve@gmail.com>
2020-02-12 14:02:59 +0100

    fix mv

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ w_dir=$(pwd)
 for file in $(find . | grep "main.tex")
 do
     dossier=$(dirname $file)
+
     cd $dossier
     docName=$(dirname $dossier)
     latexmk -pdf -interaction=nonstopmode main.tex
-    if [ "${INPUT_ARTIFACT}" == true ]; then
-          mv main.pdf ${w_dir}/Documents/$docName.pdf
-    fi
+
     cd $w_dir
+    mv ${file%tex}pdf ./Documents/$docName.pdf
 done
 
 tar -czvf Documents.tar.gz Documents/*
