dc6bff9fed4b3cf947a3133989c6d9bc0ec83828
Thomas Etcheverria <tetcheve@gmail.com>
2020-02-12 11:51:32 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ "${INPUT_ARTIFACT}" == true ]; then
+  mkdir Documents
+fi
+
+declare -A failed
+i=0
+w_dir=$(pwd)
+for file in $(find . | grep "main.tex")
+do
+    pushd $(basename $file)
+    docName=$(basename $file | sed '/\/[^\/]+//g')
+    latexmk -pdf -interaction=nonstopmode main.tex
+    if [ "${INPUT_ARTIFACT}" == true ]; then
+          mv main.pdf $w_dir"/Documents/"$docName.pdf
+    fi
+    popd
+done
+
+if [ "${INPUT_ARTIFACT}" == true ]; then
+    tar -czvf Documents.tar.gz Documents/*
+fi
+
+exit 0
