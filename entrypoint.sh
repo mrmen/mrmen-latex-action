67175684616c15236c08c0e384ee0277088a1000
Thomas Etcheverria <mrmen@users.noreply.github.com>
2020-02-12 19:36:43 +0100

    fix find

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,18 @@ mkdir Documents
 declare -A failed
 i=0
 w_dir=$(pwd)
-for file in $(find . | grep "main.tex")
+
+for file in $(find . -name "main.tex")
 do
     dossier=$(dirname $file)
-
     cd $dossier
-    docName=$(dirname $dossier)
+    
     latexmk -pdf -interaction=nonstopmode main.tex
-
+    
+    mv main.pdf $w_dir/Documents/$(dirname $file | sed 's/\.\///g; s/\//\_/g').pdf
     cd $w_dir
-    mv ${file%tex}pdf ./Documents/$docName.pdf
 done
+
 
 tar -czvf Documents.tar.gz Documents/*
 
